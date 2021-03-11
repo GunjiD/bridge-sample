de66ad3a7477334b2e6178bd08b6f7e4bd40c0b7
GunjiD <29294545a@gmail.com>
2021-03-11 22:13:35 +0900

    Ethernet ヘッダをデバッグ表示するための関数を追加

--- ltest.c
+++ ltest.c
@@ -118,3 +118,40 @@ char *my_ether_ntoa_r(u_char *hwaddr, char *buff, socklen_t size){
 
         return(buf);
 }
+
+
+
+/***
+ * Ethernet ヘッダをデバッグ表示するための関数
+ * <list>int PrintEthernetHeader(
+ * struct ether_header *eh,     Ethernet パケットのヘッダのアドレス
+ * FILE *fp                     出力先ファイルポインタ
+ * )
+ ***/
+<list>int PrintEthernetHeader(struct ether_header *eh, FILE *fp)
+{
+    char    buf[80];
+
+    fprintf(fp, "ether_header------------------------------\n");
+    fprintf(fp, "ether_dhost=%s\n", my_ether_ntoa_r(eh->ether_dhost, buf,
+                                    sizeof(buf)));
+    fprintf(fp, "ether_shost=%s\n", my_ether_ntoa_r(eh->ether_shost, buf,
+                                    sizeof(buf)));
+    fprintf(fp, "ether_type=%02X", ntohs(eh->ether_type));
+    switch (ntohs(eh->ether_type))
+    {
+    case ETH_P_IP:
+        fprintf(fp, "(IP)\n");
+        break;
+    case ETH_P_IPV6:
+        fprintf(fp, "(IPv6)\n");
+        break;
+    case ETH_P_ARP:
+        fprintf(fp, "(ARP)\n");
+        break;        
+    default:
+        fprintf(fp, "(unknown)\n");
+        break;
+    }
+    
+}
