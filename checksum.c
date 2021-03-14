a0679d35ea3338bb5a108a89ed5d0e5673e641b9
GunjiD <29294545a@gmail.com>
2021-03-14 21:31:39 +0900

    データを2つ渡してチェックサム計算を行う関数を追加

--- checksum.c
+++ checksum.c
@@ -78,10 +78,75 @@ u_int16_t checksum(u_char *data, int len)
     }
 
 
-    return(0);    
+    return(~sum);    
 }
 
-u_int16_t checksum2(u_char *data1, int len1, u_char *data2 int len2);
+
+
+/***
+ * データを2つ渡し、全体のチェックサムを計算するための関数
+ * u_int16_t checksum2(
+ * u_char *data1, 
+ * int len1, 
+ * u_char *data2 int len2
+ * )
+***/
+u_int16_t checksum2(u_char *data1, int len1, u_char *data2 int len2)
+{
+    register u_int32_t  sum;
+    register u_int16_t  *ptr;
+    register int        c;
+
+
+    sum = 0;
+    ptr = (u_int16_t *)data1;
+    for(c = len1; c > 1; c -= 2){
+        sum += (*ptr);
+        if(sum & 0x80000000){
+            sum = (sum & 0xFFFF) + (sum >> 16);
+        }
+        ptr++;
+    }
+    if(c == 1){
+        u_int16_t   val;
+        val = ((*ptr) << 8) + (*data2);
+        sum += val;
+        if(sum & 0x80000000){
+            sum = (sum & 0xFFFF) + (sum >> 16);
+        }
+        ptr = (u_int16_t *)(data2 + 1);
+        len2--;
+    }
+    else{
+        ptr = (u_int16_t *)data2;
+    }
+
+    
+    for(c = len2; c > 1; c -= 2){
+        sum += (*ptr);
+        if(sum & 0x80000000){
+            sum = (sum & 0xFFFF) + (sum >> 16);
+        }
+        ptr++;
+    }
+    if(c == 1){
+        u_int16_t   val;
+        val = 0;
+        memcpy(&val, ptr, sizeof(u_int8_t));
+        sum += val;
+    }
+
+
+    while (sum >> 16){
+        sum = (sum & 0xFFFF) + (sum >> 16);
+    }
+
+
+    return(~sum);
+}
+
+
+
 int checkIPchecksum(struct iphdr *iphdr, u_char *option, int optionLen);
 int checkIPDATAchecksum(struct iphdr *iphdr, unsigned char *data, int len);
 int checkIP6DATAchecksum(struct ip6_hdr *ip, unsigned char *data, int len);
