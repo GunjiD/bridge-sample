8e427b60d3fb3fe71f271fdcb5916ecb4c4f4afc
GunjiD <29294545a@gmail.com>
2021-03-14 17:22:13 +0900

    IPヘッダの表示関数を追加

--- print.c
+++ print.c
@@ -160,7 +160,72 @@ int PrintArp(struct ether_arp *arp, FILE *fp)
         return(0);
 }
 
-int PrintIpHeader(struct ip_header *ip_hdr, u_char *option, int optionLen, FILE *fp);
+
+
+static char     *Proto[] = {
+    "undefined",
+    "ICMP",
+    "IGMP",
+    "undefined".
+    "IPIP",
+    "undefined",
+    "TCP",
+    "undefined",
+    "EGP",
+    "undefined",
+    "undefined",
+    "undefined",
+    "PUP",
+    "undefined",
+    "undefined",
+    "undefined",
+    "undefined",
+    "UDP",
+};
+
+
+
+int PrintIpHeader(struct ip_header *ip_hdr, u_char *option, int optionLen, FILE *fp)
+{
+    int     i;
+    char    buf[80];
+
+
+    fprintf(fp, "ip------------------------------------------------\n");
+    fprintf(fp, "version=%u,", iphdr->version);
+    fprintf(fp, "ihl=%u,", iphdr->ihl);
+    fprintf(fp, "tos=%x,", iphdr->tos);
+    fprintf(fp, "tot_len=%u,", ntohs(iphdr->tot_len));
+    fprintf(fp, "id=%u\n", ntohs(iphdr->id));
+    fprintf(fp, "frag_off=%x,%u,", (ntohs(iphdr->frag_off) >> 13) & 0x07,
+            ntohs(iphdr->frag_off) & 0x1FFF);
+    fprintf(fp, "ttl=%u,", iphdr->ttl);
+    fprintf(fp, "protocol=%u", iphdr->protocol);
+    if(iphdr->protocol <= 17){
+        fprintf(fp, "(%s),", Proto[iphdr->protocol]);
+    }
+    else{
+        fprintf(fp, "(undefined),");
+    }
+    fprintf(fp, "check=%x\n", iphdr->check);
+    fprintf(fp, "saddr=%s,", ip_ip2str(iphdr->saddr, buf, sizeof(buf)));
+    fprintf(fp, "daddr=%s\n", ip_ip2str(iphdr->daddr, buf, sizeof(buf)));
+    if(optionLen > 0){
+        fprintf(fp, "option:");
+        for(i = 0; i < optionLen; i++){
+            if(i != 0){
+                fprintf(fp, ":%02x", option[i]);
+            }
+            else{
+                fprintf(fp, "%02x", option[i]);
+            }
+        }
+    }
+
+
+    return(0);
+}
+
 int PrintIp6Header(struct ip6_hdr *ip6, FILE *fp);
 int PrintIcmp(struct icmp *icmp, FILE *fp);
 int PrintIcmp6(struct icmp6_hdr *icmp6, FILE *fp);
